11d22d7bb5e4e3095a1f76e245601671cbbbb93d
vagrant <vagrant@vagrant.vm>
2020-01-28 09:17:18 +0000

    Finished.

--- server.c
+++ server.c
@@ -9,11 +9,9 @@
 #include <time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <fcntl.h> // for open
-#include <unistd.h> // for close
-#define MYPORT 5000 /* Avoid reserved ports */
-#define BACKLOG 5  /* pending connections queue size */
-#define ERROR_404_MESSAGE "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD><BODY><H1>404 Not Found</H1><P>The requested file was not found on this server.</P></BODY></HTML>"
+#include <fcntl.h> 
+#include <unistd.h> 
+#define BACKLOG 5  
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
@@ -30,19 +28,15 @@ void strip_ext(char *fname);
 
 int main(int argc, char *argv[])
 {
-  struct sockaddr_in my_addr;    /* my address */
-  struct sockaddr_in their_addr; /* connector addr */
+  struct sockaddr_in my_addr;  
+  struct sockaddr_in their_addr; 
   socklen_t sin_size;
-  /* create a socket */
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
     perror("socket");
     exit(1);
   }
-
-  // ...
   bzero((char *)&my_addr, sizeof(my_addr));
-  /* set the address info */
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(8000); 
   my_addr.sin_addr.s_addr = INADDR_ANY;
@@ -59,25 +53,6 @@ int main(int argc, char *argv[])
   }
   while (1)
   { 
-	/*char buffer[1024];
-	bzero(buffer, 1024);
-    sin_size = sizeof(their_addr);
-    if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1)
-    {
-      perror("accept");
-      exit(0);
-    }
-	if (read(new_fd, buffer, 1023) < 0)
-	{
-		perror("Error from reading socket");
-		exit(1);
-	}
-	
-	printf("%s\n", buffer);
-	
-	char* filename = parser(buffer);
-	send_file(filename,new_fd);
-	close(new_fd);*/
 	if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1)
     {
       perror("accept");
@@ -108,13 +83,14 @@ int main(int argc, char *argv[])
     
   }
   close(sockfd);
+  return 0;
 }
 
 char* parser(char* msg)
 {
-	const char delim[1] = " ";
-	char* token = strtok(msg, delim);
-	token = strtok(NULL, delim);
+	const char space[1] = " ";
+	char* token = strtok(msg, space);
+	token = strtok(NULL, space);
 	token ++;
 
 	int* spacePtr = strstr(token, "%20");
@@ -156,8 +132,10 @@ void send_file(char* filename, int sockfd)
 
 	if (ptr == NULL)
 	{
+		char* not_found = "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD><BODY><H1>404 Not Found</H1><P>The requested file was not found on this server.</P></BODY></HTML>";
+		int len = strlen(not_found);
 		send_response(sockfd, 404, filename, 0);
-		send(sockfd, ERROR_404_MESSAGE, strlen(ERROR_404_MESSAGE), 0);
+		send(sockfd, not_found, len, 0);
 		return;
 	}
 	if (!fseek(ptr, 0, SEEK_END))
@@ -165,9 +143,11 @@ void send_file(char* filename, int sockfd)
 		long size = ftell(ptr);
 		if (size < 0)
 		{
+			char* not_found = "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD><BODY><H1>404 Not Found</H1><P>The requested file was not found on this server.</P></BODY></HTML>";
+			int len = strlen(not_found);
 			send_response(sockfd, 404, filename, 0);
-			send(sockfd, ERROR_404_MESSAGE, strlen(ERROR_404_MESSAGE), 0);
-			return;
+			send(sockfd, not_found, len, 0);
+			return;	
 		}
 		fseek(ptr, 0, SEEK_SET);
 		char* buffer = malloc(sizeof(char) * (size + 1));
@@ -215,92 +195,82 @@ char *replaceWord(char *s, char *oldW, char *newW)
 } 
 
 void send_response(int socket, int code, char* file_name, int file_length)
-{
-	char* header;
-	if (code == 404)
-		header = "HTTP/1.1 404 Not Found\r\n";
-	else
-		header = "HTTP/1.1 200 OK\r\n";
-	char *connection = "Connection: close\r\n";
-	char *server = "Server: Yiv/1.0\r\n";
-	time_t raw_time;
-    time(&raw_time);
-    struct tm *time_info = localtime(&raw_time);
-    char current_time[32];
-    strftime(current_time, 32, "%a, %d %b %Y %T %Z", time_info);
-    char date[64] = "Date: ";
-    strcat(date, current_time);
-    strcat(date, "\r\n");
-	
-	struct stat attr;
-    stat(file_name, &attr);
-    //st_mtime gives the modification time
-    struct tm* lm_info = localtime(&(attr.st_mtime));
-    char lm_time[32];
-    strftime(lm_time, 32, "%a, %d %b %Y %T %Z", lm_info);
-    char last_modified[64] = "Last-Modified: ";
-    strcat(last_modified, lm_time);
-    strcat(last_modified, "\r\n");
-	
-	//content-length section
-    char content_length[50] = "Content-Length: ";
-    char len_buffer[16];
-    sprintf(len_buffer, "%d", file_length);
-    strcat(content_length, len_buffer);
-    strcat(content_length, "\r\n");
-	
-	char *dot;
-    char *content_type = "Content-Type: text/html\r\n";
-    if ((dot = strrchr(file_name, '.')) != NULL){
-		if (strcasecmp(dot, ".jpeg") == 0)
-            content_type = "Content-Type: image/jpeg\r\n";
-        else if (strcasecmp(dot, ".gif") == 0)
-            content_type = "Content-Type: image/gif\r\n";
-		else if (strcasecmp(dot, ".png") == 0)
-			content_type = "Content-Type: image/png\r\n";
-		else if (strcasecmp(dot, ".jpg") == 0)
-			content_type = "Content-Type: image/jpg\r\n";
-		else if (strcasecmp(dot, ".htm") == 0)
-			content_type = "Content-Type: text/htm\r\n";
-		else if (strcasecmp(dot, ".txt") == 0)
-			content_type = "Content-Type: text/txt\r\n";
+{	
+	char *dotPos;
+    char *type;
+    if ((dotPos = strrchr(file_name, '.')) != NULL){
+		if (strcasecmp(dotPos, ".html") == 0)
+			type = "Content-Type: text/html\r\n";
+		else if (strcasecmp(dotPos, ".jpeg") == 0)
+            type = "Content-Type: image/jpeg\r\n";
+        else if (strcasecmp(dotPos, ".gif") == 0)
+            type = "Content-Type: image/gif\r\n";
+		else if (strcasecmp(dotPos, ".png") == 0)
+			type = "Content-Type: image/png\r\n";
+		else if (strcasecmp(dotPos, ".jpg") == 0)
+			type = "Content-Type: image/jpg\r\n";
+		else if (strcasecmp(dotPos, ".htm") == 0)
+			type = "Content-Type: text/htm\r\n";
+		else if (strcasecmp(dotPos, ".txt") == 0)
+			type = "Content-Type: text/txt\r\n";
+		else
+			type = "Content-Type: text/html\r\n";
 	}
 	else
 		if (code != 404)
-			content_type = "Content-Type: application/octet-stream";
-		
-	char hdr_section[6][50];
-    strcpy(hdr_section[0], connection);
-    strcpy(hdr_section[1], date);
-    strcpy(hdr_section[2], server);
-    strcpy(hdr_section[3], last_modified);
-    strcpy(hdr_section[4], content_length);
-    strcpy(hdr_section[5], content_type);
-
-    //construct the message
-    char msg[1024];
-
-    //copy status section
-    int position = strlen(header);
-    memcpy(msg, header, position);
+			type = "Content-Type: application/octet-stream";
+		else
+			type = "Content-Type: text/html\r\n";
 	
-	 //copy other sections
-    int i;
-	int newcode = -1;
-	if (code == 200)
-		newcode = 0;
-    for (i = 0; i < 6; i++)
-    {
-        if (newcode == 0 || i == 1 || i == 2 || i == 5)
-        {
-            memcpy(msg + position, hdr_section[i], strlen(hdr_section[i]));
-            position += strlen(hdr_section[i]);
-        }
-    }
+	time_t rawtime;
+	struct tm * timeinfo;
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+    char current[64];
+	char timestamp[64] = "Date: ";
+    strftime(current, 64, "%a, %d %b %Y %T %Z", timeinfo);
+    strcat(current, "\r\n");
+	strcat(timestamp, current);
+	
+	char modifed[32];
+	struct stat b;
+	char lm[64] = "Last-Modified: ";
+	if (!stat(file_name, &b)){
+		strftime(modifed, 100, "%a, %d %b %Y %T %Z", localtime( &b.st_mtime));
+		strcat(modifed, "\r\n");
+		strcat(lm, modifed);
+	}
+	
+    char fileLength[50] = "Content-Length: ";
+    char len[30];
+    sprintf(len, "%d", file_length);
+    strcat(len, "\r\n");
+	strcat(fileLength, len);
+	
+    char msg[1024] = "";
+	char *connection = "Connection: close\r\n";
+	char *server = "Server: Yiv/1.0\r\n";
 
-    memcpy(msg + position, "\r\n\0", 3);
-
-    //send and print completed response message
+	if (code == 404)
+	{
+		strcat(msg, "HTTP/1.1 404 Not Found\r\n");
+		printf("%s\n", timestamp);
+		strcat(msg, timestamp);
+		strcat(msg, server);
+		strcat(msg, type);
+		strcat(msg, "\r\n\0");
+	}
+	else if (code == 200)
+	{
+		strcat(msg, "HTTP/1.1 200 OK\r\n");
+		strcat(msg, connection);
+		strcat(msg, timestamp);
+		strcat(msg, server);
+		strcat(msg, lm);
+		strcat(msg, fileLength);
+		strcat(msg, type);
+		strcat(msg, "\r\n\0");
+	}
     send(socket, msg, strlen(msg), 0);
 }
 
